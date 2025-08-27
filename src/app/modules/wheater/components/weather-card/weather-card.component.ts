070345af2415c98d614ee1c2036e504ac147507d
hayghlander <hayghlanderuitec@gmail.com>
2025-08-27 19:31:46 -0300

    projeto final weather-app

--- src/app/modules/wheater/components/weather-card/weather-card.component.ts
+++ src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -1,15 +1,18 @@
 import { WeatherDatas } from 'src/app/models/interfaces/weather.interface';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: [],
 })
-export class WeatherCardComponent implements OnInit {
+export class WeatherCardComponent {
   @Input() weatherDatasInput!: WeatherDatas;
 
-  ngOnInit(): void {
-    console.log('DADOS RECEBIDOS DO COMPONENTE PAI', this.weatherDatasInput);
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
   }
-}
+
